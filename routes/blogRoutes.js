a5c8433c381151026e1e3481a3742f2e1311edde
divantso <divantso@cisco.com>
2019-10-10 20:16:12 +0200

    Add methods get and post to the /blogs route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,37 @@
 const Post = require('../models/Post');
+const mongoose = require('mongoose');
 
 
 module.exports = (app) => {
-  
+  app.get('/blogs', async (req, res) => {
+    try {
+      const posts = await Post.find({});
+      if(!posts.length) {
+        return res.status(200).json({message: 'No posts are currently in the database'});
+      }
+      return res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: posts
+      });
+    } catch (err) {
+      return res.json({message: `Error: ${err}`});  
+    }
+  });
+
+  app.post('/blogs', async (req, res) => {
+    try {
+      const newPost = new Post({
+        id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        content: req.body.content
+      });
+      await newPost.save();
+      return res.status(201).json({
+        message: 'Post has been successfully sent to the database',
+        post: newPost
+      });
+    } catch (err) {
+      res.json({message: `Error ${err}`});
+    }
+  })
 };
