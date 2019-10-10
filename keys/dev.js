a4cdaf1bb020cdddad6ff12d34239b3ea1d21933
divantso <divantso@cisco.com>
2019-10-10 17:33:37 +0200

    Generate secret cookieKey and modngo URL

---
+++ keys/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  cookieKey: '123123123',
+  mongoUrl: 'mongodb://lens_pro:lens_pro@cluster0-shard-00-00-hi33z.mongodb.net:27017,cluster0-shard-00-01-hi33z.mongodb.net:27017,cluster0-shard-00-02-hi33z.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
