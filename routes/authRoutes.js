256ab593980d2805bb83afad7ce548eaca1f266d
divantso <divantso@cisco.com>
2019-10-16 10:22:53 +0200

    Add passport login method call to signup route, remove logic, that is repeating passport authenticating strategy, on login route. Add logout route.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,22 +11,25 @@ module.exports = (app) => {
       const user = await User.findOne({
         email: req.body.email
       });
-      console.log('user', user)
       if(user) {
         return res.status(409).json({
           message: "The user with this email already exists"
         });
       } else {
         const hashedPassword = await bcrypt.hash(req.body.password, 10).catch(err => res.status(500).json({error: err}));
-        const newUser = new User({
+        const user = new User({
           id: new mongoose.Types.ObjectId(),
+          name: req.body.name,
           email: req.body.email,
           password: hashedPassword
         });
-        await newUser.save();
-        return res.status(201).json({
-          message: 'User was successfully signed up',
-          user: user
+        await user.save();
+        req.login(user, (err) => {
+          if(err) console.log(err);
+          return res.status(201).json({
+            message: 'User was successfully signed up',
+            user: user
+          });
         })
       }
     } catch(error) {
@@ -34,24 +37,19 @@ module.exports = (app) => {
     }
   });
 
-  app.post('/login', passport.authenticate('local'), async(req, res, next) => {
+  app.post('/login', passport.authenticate('local', { failureMessage: "Invalid username or password" }), (req, res, next) => {
     try {
-      console.log(req.body.email)
-      const user = await User.findOne({
-        email: req.body.email,
+      res.status(200).json({
+        message: 'Auth successful',
+        user: req.user
       });
-      if(!user) {
-        return res.status(401).json({message: 'Auth failed, user wasn\'t found'})
-      }
-      const passCheck = bcrypt.compare(req.body.password, user.password).catch(err => res.status(401).json({message: 'Auth failed, user password doesn\'t match'}));
-      if(passCheck) {
-        return res.status(200).json({
-          user: user,
-          message: 'Auth successful'
-        });
-      }
     } catch (error) {
-      return res.json({message: `smth went wrong ${error}`})
+      return res.status(401).json({message: `smth went wrong ${error}`})
     }
+  });
+
+  app.get('/logout', (req, res) => {
+    req.logout();
+    return res.status(200).json({message: 'User logged out'});
   })
 };
