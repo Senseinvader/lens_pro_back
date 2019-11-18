9a18b34a251fdafd86f0842b6b7a3c0fe0b6491c
divantso <divantso@cisco.com>
2019-11-18 12:04:02 +0100

    Fix the clearHash function, remove console logs

--- services/cache.js
+++ services/cache.js
@@ -3,16 +3,12 @@ const redis = require('redis');
 const util = require('util');
 const clientUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(clientUrl);
-client.get = util.promisify(client.get);
 client.hget = util.promisify(client.hget);
-client.set = util.promisify(client.set);
-client.hset = util.promisify(client.hset);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
   this.hashKey = JSON.stringify(options.key || '');
-  console.log('hash key',this.hashKey);
   return this;
 }
 
@@ -21,7 +17,6 @@ mongoose.Query.prototype.exec = async function () {
   // If a query is not cached yet
   if(!this.useCache) {
     const result = await exec.apply(this, arguments);
-    console.log('data from db with no cache', result.length)
     return result;
   }
 
@@ -35,7 +30,6 @@ mongoose.Query.prototype.exec = async function () {
   const cachedData = await client.hget(this.hashKey, key).catch(err => console.log(err));
   if(cachedData) {
     const doc = JSON.parse(cachedData);
-    console.log('cachedData',doc.length, this.hashKey)
 
     return Array.isArray(doc) 
       ? doc.map(el => new this.model(el))
@@ -46,13 +40,12 @@ mongoose.Query.prototype.exec = async function () {
   const result = await exec.apply(this, arguments);
 
   // And set the data to the Redis to store it for the future
-  console.log('data from db', result)
   client.hset(this.hashKey, key, JSON.stringify(result));
   return result;
 }
 
 module.exports = {
   clearHash(hashKey) {
-    JSON.stringify(client.del(hashKey));
+    client.del(JSON.stringify(hashKey));
   }
 }
